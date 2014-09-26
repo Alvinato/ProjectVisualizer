a063f3db8b854c80b76a30201aadd8fd2815c894
Jason Pinto <jason.pinto15@gmail.com>
2014-09-26 12:24:27 -0700

    upgraded git metrics functions to require filepath

--- controllers/gitmetrics.py
+++ controllers/gitmetrics.py
@@ -1,16 +1,19 @@
 import subprocess
 from collections import OrderedDict
+import git
 import sys
 import os
 
 
-def get_contributors_for_file(filename):
+def get_contributors_for_file(file_path, filename):
     """Returns an ordered dictionary that represents the author for every single
     line of code in a file. Mapping is line_number:author email"""
     output = subprocess.check_output("git blame  --show-email {} "
                                      "| grep -o -p '<.*>' "
                                      "| cut -d '>' -f1"
-                                     "| cut -d '<' -f2".format(filename), shell=True)
+                                     "| cut -d '<' -f2".format(filename),
+                                     shell=True,
+                                     cwd=r'{}'.format(file_path))
     output_array = output.split('\n')
     num_lines = len(output_array)
     line_number = 1
@@ -20,14 +23,16 @@ def get_contributors_for_file(filename):
         line_number = line_number + 1
     return line_contributors
 
-def get_contributor_for_line(filename, line_number):
+def get_contributor_for_line(file_path, filename, line_number):
     "Get the email for an author of a specific line of code"
     contributor = subprocess.check_output("git blame -L {},{} --show-email {} "
                                      "| grep -o -p '<.*>' "
                                      "| cut -d '>' -f1"
-                                     "| cut -d '<' -f2".format(line_number,line_number, filename), shell=True)
+                                     "| cut -d '<' -f2".format(line_number,line_number, filename),
+                                     shell=True,
+                                     cwd=r'{}'.format(file_path))
     return contributor
 
 if __name__ == "__main__":
-    #contributors = get_contributors_for_file('/Users/jasonpinto/Downloads/web2py/applications/jaat/controllers/appadmin.py')
-    get_contributor_for_line('/Users/jasonpinto/Downloads/web2py/applications/jaat/controllers/appadmin.py', 1)
+    "NOTE: Just use for testing purposes"
+    print get_contributor_for_line('/Users/jasonpinto/plumbum/plumbum', '/Users/jasonpinto/plumbum/plumbum/version.py', 1)
