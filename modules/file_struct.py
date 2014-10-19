48fcef9d446946a2dbe219c51a2556c47d189cee
Arjun Sumal <v0o7@ugrad.cs.ubc.ca>
2014-10-19 03:20:32 -0700

    got file structure of code base

--- modules/file_struct.py
+++ modules/file_struct.py
@@ -11,7 +11,8 @@ class Code_Structure(object):
     def __init__(self, name, root):
         self.name = name
         self.root = root
-        self.path = self.create_path(root)
+        self.path = self.create_path_04(root)
+        print self.path
 
     def print_file_structure(self, startpath):
          """ http://stackoverflow.com/questions/
@@ -30,6 +31,42 @@ class Code_Structure(object):
 
     def get_root(self):
         return self.root
+
+    def create_path_04(self, path):
+        structure = {}
+        root, subdirs, files = next(os.walk(path))
+        for file in files:
+            structure.update({file : None})
+        for subdir in subdirs:
+            new_path = "%s\%s" % (path, subdir)
+            structure.update({subdir : self.create_path_04(new_path)})
+        return structure
+
+    def create_path_03(self, path):
+        structure = {}
+        root, subdirs, files = next(os.walk(path))
+        print "PATH", path
+        print "SUBDIR", subdirs
+        print "FILES", files
+        print ""
+
+        for subdir in subdirs:
+            new_path = "%s\%s" % (path, subdir)
+            structure.update({subdir : self.create_path_03(new_path)})
+        file_structure = {}
+        for file in files:
+            file_structure.update({file : None})
+        structure.update({path : file_structure})
+        return structure
+
+    def create_path_02(self, root):
+        print "****START****"
+        code_structure = {}
+        for path, subdirs, files in os.walk(root):
+            print "PATH", path
+            print "SUBDIR", subdirs
+            print "FILES", files
+            print ""
 
     def create_path(self, root):
         """ Given the root directory, this function will return
