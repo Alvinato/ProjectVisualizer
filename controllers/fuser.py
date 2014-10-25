ffc5c2d82505b1f6cc7b101379249afa44dd4d25
Jason Pinto <jason.pinto15@gmail.com>
2014-10-25 15:34:35 -0700

    added debugging option inside config.json

--- controllers/fuser.py
+++ controllers/fuser.py
@@ -3,17 +3,18 @@ from filehelper import find_python_files_in_project
 from authormapper import get_contributors_for_file
 from filehelper import convert_file_to_json
 import os
+import json
 class Fuser():
 
     config = {}
 
     def __init__(self):
-        self.config = load_project_properties();
+        self.config = load_project_properties()
 
 
     def generate_project_object(self):
         project_object = []
-        files = find_python_files_in_project(str(self.config['pattern_location']))
+        files = find_python_files_in_project(str(self.config['plumbum_location']))
         for file in files:
             if file != '':
                 file_dict = convert_file_to_json(file)
@@ -21,7 +22,11 @@ class Fuser():
                 authors = get_contributors_for_file(file_path, file)
                 for key in file_dict[file].keys():
                     file_dict[file][key]['author'] = authors[key]
+
                 project_object.append(file_dict)
+        if self.config['debug'] == 'on':
+            with open('data.json', 'w') as outfile:
+                json.dump(project_object, outfile, sort_keys = True, indent = 4)
         return project_object
 
 
