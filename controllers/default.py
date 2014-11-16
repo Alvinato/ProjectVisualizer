a851cb0cdc02ae5ea9025e6dec5954d1245e0c7c
Arjun Sumal <arjunsumal@gmail.com>
2014-11-16 12:02:33 -0800

    getting pattern .json files ready

--- controllers/default.py
+++ controllers/default.py
@@ -13,14 +13,14 @@ def final():
     path_to_plumbum = "/home/asumal/git/cs410/plumbum/plumbum"
     path_to_pattern = "/home/asumal/git/cs410/pattern/pattern"
 
-    list_of_files = FH.find_python_files_in_project(path_to_plumbum)
+    list_of_files = FH.find_python_files_in_project(path_to_pattern)
     pylint_analysis = PY.get_pylint_analysis(list_of_files)
-    git_analysis = AM.get_git_analysis(path_to_plumbum, list_of_files)
+    git_analysis = AM.get_git_analysis(path_to_pattern, list_of_files)
 
     for python_file in list_of_files:
-        result = FR.fuse_file(path_to_plumbum, python_file, pylint_analysis[python_file], git_analysis[python_file])
-        FR.save_file(result, python_file, PLUMBUM)
+        result = FR.fuse_file(path_to_pattern, python_file, pylint_analysis[python_file], git_analysis[python_file])
+        FR.save_file(result, python_file, PATTERN)
 
-    solution = FS.create_structure(path_to_plumbum, PLUMBUM, pylint_analysis, git_analysis, PLUMBUM)
+    solution = FS.create_structure(path_to_plumbum, PATTERN, pylint_analysis, git_analysis, PATTERN)
 
     return dict(solution = solution)
--- modules/filestructure.py
+++ modules/filestructure.py
@@ -40,10 +40,10 @@ if __name__ == '__main__':
     import authormapper as AM
     import pylintanalzyer as PY
 
-    path_to_code_base = "/home/asumal/git/cs410/plumbum/plumbum"
+    path_to_code_base = "/home/asumal/git/cs410/pattern/pattern"
 
     list_of_files = FH.find_python_files_in_project(path_to_code_base)
     pylint_analysis = PY.get_pylint_analysis(list_of_files)
     git_analysis = AM.get_git_analysis(path_to_code_base, list_of_files)
 
-    print json.dumps(create_structure(path_to_code_base, "plumbum", pylint_analysis, git_analysis, "plumbum"))
+    print json.dumps(create_structure(path_to_code_base, "pattern", pylint_analysis, git_analysis, "pattern"))
